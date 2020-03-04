49f8a110fd65cb776222e4653dad9b4b76f59733
Anthony Iozzia <anthony.iozzia@epfl.ch*>
2020-03-04 23:39:10 +0100

    style 2

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -1,5 +1,5 @@
 package ch.epfl.sdp;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -7,6 +7,7 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
