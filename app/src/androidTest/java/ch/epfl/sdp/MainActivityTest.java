8b5c93a1b8847e00abb837523c97f6884340fa7c
Anthony Iozzia <anthony.iozzia@epfl.ch*>
2020-03-04 23:34:15 +0100

    style

--- app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.sdp;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -8,7 +7,7 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
+import androidx.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +19,8 @@ public class MainActivityTest {
             new ActivityTestRule<>(MainActivity.class);
     @Test
     public void testCanGreetUsers() {
-        onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
+        onView(withId(R.id.mainName)).perform(typeText("from my unit test"))
+                .perform(closeSoftKeyboard());
         onView(withId(R.id.mainGoButton)).perform(click());
         onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
     }
